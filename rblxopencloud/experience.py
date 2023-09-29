38be76cb495cb16ed7e496ab2635a92c50743268
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2023-09-29 20:34:38 +1000

    create experience api reference

--- rblxopencloud/experience.py
+++ rblxopencloud/experience.py
@@ -10,15 +10,15 @@ __all__ = (
 
 class Experience():
     """
-    Represents an experience/game object on Roblox. This class allows interaction with an experience's data stores, messaging service, and uploading place files.
+    Represents an experience/game on Roblox. Allows interaction with data stores, messaging service, etc.
 
-    ### Example
-    ```py
-    experience = rblxopencloud.Experience(123456789, "your-api-key")
-    ```
-    ### Paramaters
-    id: int - The experience's (aka universe's) ID. See [this Roblox documentation](https://create.roblox.com/docs/cloud/open-cloud/data-store-api-handling#universe-id) to find yours.
-    api_key: str - :param str api_key: Your API key created from [Creator Dashboard](https://create.roblox.com/credentials) with access to this experience.
+    Args:
+        id: The experience/universe ID
+        api_key: The API key created on the [Creator Dashboard](https://create.roblox.com/credentials) with access to the experience.
+    
+    Attributes:
+        id (int): The experience/universe ID
+        owner (Optional[Union[User, Group]]): The object of the experience owner. Only present from OAuth2.
     """
 
     def __init__(self, id: int, api_key: str):
@@ -31,52 +31,59 @@ class Experience():
     
     def get_data_store(self, name: str, scope: Optional[str]="global") -> DataStore:
         """
-        Creates a `rblx-open-cloud.DataStore` which allows interaction with a data store from the experience. `DataStore.created` will be `None`.
+        Creates a `rblxopencloud.DataStore` with the provided name and scope. This function doesn't make an API call so there is no validation.
 
-        Lua equivalent: [DataStoreService:GetDataStore()](https://create.roblox.com/docs/reference/engine/classes/DataStoreService#GetDataStore)
-        ### Example
-        ```py
-        data_store = Experience.get_data_store(name="playerExperience", scope="global")
-        ```
-        ### Parameters
-        name: str - The data store name.
-        scope: Optional[str] - The data store scope. Defaults to global, and can be `None` for key syntax like `scope/key`.
+        Args:
+            name: The data store name.
+            scope: The data store scope. Set it to none for `scope/key` syntax.
+        
+        Returns:
+            The Data Store with the provided name and scope.
         """
         return DataStore(name, self, self.__api_key, None, scope)
     
     def get_ordered_data_store(self, name: str, scope: Optional[str]="global") -> OrderedDataStore:
         """
-        Creates a `rblx-open-cloud.OrderedDataStore` which allows interaction with an ordered data store from the experience.
+        Creates a `rblxopencloud.OrderedDataStore` with the provided name and scope. This function doesn't make an API call so there is no validation.
 
-        Lua equivalent: [DataStoreService:GetOrderedDataStore()](https://create.roblox.com/docs/reference/engine/classes/DataStoreService#GetOrderedDataStore)
-        ### Example
-        ```py
-        data_store = Experience.get_ordered_data_store(name="playerWins", scope="global")
-        ```
-        ### Parameters
-        name: str - The data store name.
-        scope: Optional[str] - The data store scope. Defaults to global, and can be `None` for key syntax like `scope/key`.
+        Args:
+            name: The data store name.
+            scope: The data store scope. Set it to none for `scope/key` syntax.
+
+        Returns:
+            The Ordered Data Store with the provided name and scope.
         """
         return OrderedDataStore(name, self, self.__api_key, scope)
 
     def list_data_stores(self, prefix: Optional[str]="", limit: Optional[int]=None, scope: Optional[Union[str, None]]="global") -> Iterable[DataStore]:
         """
-        Interates `rblx-open-cloud.DataStore` for all of the Data Stores in the experience.
+        Interates `rblxopencloud.DataStore` for all of the Data Stores in the experience.
 
-        Lua equivalent: [DataStoreService:ListDataStoresAsync()](https://create.roblox.com/docs/reference/engine/classes/DataStoreService#ListDataStoresAsync)
-        ### Example
-        ```py
-        for datastore in experience.list_data_stores():
-            print(datastore)
-        ```
-        You can get the data stores in a list like this:
-        ```py
-        list(experience.list_data_stores())
-        ```
-        ### Parameters
-        prefix: Optional[str] - Only return Data Stores with names starting with this value.
-        limit: Optional[int] - The maximum number of Data Stores to iterate.
-        scope: Optional[str] - The scope for all data stores. Defaults to global, and can be `None` for key syntax like `scope/key`.
+        Example:
+            This will print every data store in the experience. 
+            ```py
+            for datastore in experience.list_data_stores():
+                print(datastore)
+            ```
+            If you'd like the datastores in a list, you can use the list method:
+            ```py
+            list(experience.list_data_stores())
+            ```
+
+        Args:
+            prefix: Only return Data Stores with names starting with this value.
+            limit: The maximum number of Data Stores to iterate.
+            scope: The scope for all data stores. Defaults to global, and can be `None` for key syntax like `scope/key`.
+        
+        Returns:
+            An Iterable of `rblxopencloud.DataStore` in the experience.
+
+        Raises:
+            InvalidKey: The API key isn't valid, doesn't have access to list data stores, or is from an invalid IP address.
+            NotFound: The experience does not exist.
+            RateLimited: You've exceeded the rate limits.
+            ServiceUnavailable: The Roblox servers ran into an error, or are unavailable right now.
+            rblx_opencloudException: Roblox returned an unexpected error.
         """
         nextcursor = ""
         yields = 0
@@ -104,17 +111,21 @@ class Experience():
         """
         Publishes a message to live game servers that can be recieved with [MessagingService](https://create.roblox.com/docs/reference/engine/classes/MessagingService).
 
-        The `universe-messaging-service:publish` scope is required for OAuth2 authorization.
+        **The `universe-messaging-service:publish` scope is required for OAuth2 authorization.**
 
-        Messages sent by Open Cloud with only be recieved by live servers. Studio won't recieve thesse messages.
-
-        ### Example
-        ```py
-        experience.publish_message("exampleTopic", "Hello World!")
-        ```
-        ### Parameters
-        topic: str - The topic to send the message in
-        data: str - The message to send. Open Cloud does not support sending dictionaries/tables with publishing messages. You'll have to json encode it before sending it, and decode it in Roblox.
+        Args:
+            topic: The topic to send the message in
+            data: The message to send. Open Cloud does not support sending dictionaries/tables with publishing messages. You'll have to json encode it before sending it, and decode it in Roblox.
+        
+        Raises:
+            InvalidKey: The API key isn't valid, doesn't have access to publish messages, or is from an invalid IP address.
+            NotFound: The experience does not exist.
+            RateLimited: You've exceeded the rate limits.
+            ServiceUnavailable: The Roblox servers ran into an error, or are unavailable right now.
+            rblx_opencloudException: Roblox returned an unexpected error.
+        
+        !!! note
+            Messages sent by Open Cloud with only be recieved by live servers. Studio won't recieve thesse messages.
         """
         response = request_session.post(f"https://apis.roblox.com/messaging-service/v1/universes/{self.id}/topics/{topic}",
         json={"message": data}, headers={"x-api-key" if not self.__api_key.startswith("Bearer ") else "authorization": self.__api_key, "user-agent": user_agent})
@@ -127,17 +138,29 @@ class Experience():
     
     def upload_place(self, place_id:int, file: io.BytesIO, publish: Optional[bool] = False) -> int:
         """
-        Uploads the place file to Roblox and returns the new version number.
+        Uploads the place file to Roblox, optionally choosing to publish it.
 
-        ### Example
-        ```py
-        with open("example.rbxl", "rb") as file:
-            experience.upload_place(1234, file, publish=False)
-        ```
-        ### Parameters
-        place_id: int - The place ID to upload the file to.
-        file: io.BytesIO - The file to upload. The file should be opened in bytes.
-        publish: Optional[bool] - Wether to publish the place as well. Defaults to `False`.
+        Example:
+            Replace `0000000` with the target place's ID, and `example.rbxl` should be the path to the roblox place file.
+            ```py
+            with open("example.rbxl", "rb") as file:
+                experience.upload_place(0000000, file, publish=False)
+            ```
+
+        Args:
+            place_id: The place ID to upload the file to.
+            file: The place file to upload, opened in bytes.
+            publish: Wether to publish the new place file.
+
+        Returns:
+            The place's new version ID.
+        
+        Raises:
+            InvalidKey: The API key isn't valid, doesn't have access to publish places, or is from an invalid IP address.
+            NotFound: The experience does not exist.
+            RateLimited: You've exceeded the rate limits.
+            ServiceUnavailable: The Roblox servers ran into an error, or are unavailable right now.
+            rblx_opencloudException: Roblox returned an unexpected error.
         """
         response = request_session.post(f"https://apis.roblox.com/universes/v1/{self.id}/places/{place_id}/versions",
             headers={"x-api-key": self.__api_key, 'content-type': 'application/octet-stream', "user-agent": user_agent}, data=file.read(), params={
