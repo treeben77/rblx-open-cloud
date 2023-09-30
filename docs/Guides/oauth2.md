711054476ec5de10ec48152d46d484632c817c05
TreeBen77 <77905642+TreeBen77@users.noreply.github.com>
2023-09-30 19:04:02 +1000

    add oauth2 and user ref (excl. creator)

--- docs/Guides/oauth2.md
+++ docs/Guides/oauth2.md
@@ -35,13 +35,13 @@ from rblxopencloud import OAuth2App
 rblxapp = OAuth2App(0000000000000000000, "your-client-secret", "https://example.com/redirect")
 ```
 
-Change `0000000000000000000` to your app's client ID, `your-client-secret` to your app's client secret, and `https://example.com/redirect` to the redirect URI you'll use for testing that you configured in the dashboard. You've created an `OAuth2App` which will be used for generating redirect URIs, and processing OAuth2 requests.
+Change `0000000000000000000` to your app's client ID, `your-client-secret` to your app's client secret, and `https://example.com/redirect` to the redirect URI you'll use for testing that you configured in the dashboard. You've created a [`rblxopencloud.OAuth2App`][rblxopencloud.OAuth2App] which can be used for generating redirect URIs, and processing OAuth2 requests.
 
 ## Basic OAuth2 Flow
 
 ### Redirecting Users to Consent Page
 
-The first part of the OAuth2 flow is redirecting users to Roblox's consent page. You can create the redirection URI yourself, however the library has one built in which makes everything cleaner and easier. To generate a redirect URI you can use this:
+The first part of the OAuth2 flow is redirecting users to Roblox's consent page. You can create the redirection URI yourself, however the library has one built in which makes everything cleaner and easier. To generate a redirect URI you can use [`OAuth2App.generate_uri`][rblxopencloud.OAuth2App.generate_uri]:
 
 ```py
 rblxapp.generate_uri(['openid', 'profile'])
@@ -67,11 +67,11 @@ After the user has authorized your app on the consent page, Roblox will redirect
 ```
 https://example.com/redirect?code=examplecode&state=yourstatehere
 ``` 
-You will need to extract that code from the URI and pass it through the exchange code method shown below:
+You will need to extract that code from the URI and pass it through [`OAuth2App.exchange_code`][rblxopencloud.OAuth2App.exchange_code], as shown below:
 ```py
 access = rblxapp.exchange_code("examplecode")
 ```
-This will return an `AccessToken`, which will be explained below. If you're using flask, a basic set up could look like this:
+This will return an [`rblxopencloud.AccessToken`][rblxopencloud.AccessToken], which will be explained below. If you're using flask, a basic set up could look like this:
 
 ```py
 @app.route('/redirect')
@@ -81,9 +81,9 @@ def redirect():
 
 ### Accessing Authorized Data
 
-All authorized data can be accessed from the `AccessToken` returned by `exchange_code`. If you used the `openid` scope, you can access the user's ID using `access.user.id`. If you also used the `profile` scope, you can access other user info such as `access.user.username` and `access.user.headshot_uri`. However if you've requested access to the user's resources, it gets a little bit more complex.
+All authorized data can be accessed from the [`rblxopencloud.AccessToken`][rblxopencloud.AccessToken]. If you used the `openid` scope, you can access the user's ID using `access.user.id`. If you also used the `profile` scope, you can access other user info such as `access.user.username` and `access.user.headshot_uri`. However if you've requested access to the user's resources, it gets a little bit more complex.
 
-If you've request access to the user's inventory (`user.inventory-item:read`) or groups (`group:read`), you can use the methods inside of `access.user`, for example like this:
+If you've request access to the user's inventory (`user.inventory-item:read`) or groups (`group:read`), you can use the methods inside of [`AccessToken.user`][rblxopencloud.AccessToken], for example like this:
 ```py
 for item in access.user.list_inventory():
     print(item)
@@ -98,7 +98,7 @@ However, things get a little bit more complex if you asked for permissions for t
 
 ### Accessing Resources
 
-When requesting scopes for experiences, or accounts (users and groups), the user chooses what experiences/accounts get that scope. For example, the use may want to provide you access to upload asset's to their group but not personal account. After exchanging the code, you have to use `fetch_resources` to see what accounts and experiences were authorized. Here's an example:
+When requesting scopes for experiences, or accounts (users and groups), the user chooses what experiences/accounts get that scope. For example, the use may want to provide you access to upload asset's to their group but not personal account. After exchanging the code, you have to use [`AccessToken.fetch_resources`][rblxopencloud.AccessToken.fetch_resources] to see what accounts and experiences were authorized. Here's an example:
 ```py
 resources = access.fetch_resources()
 
@@ -107,7 +107,7 @@ print(resources.experiences)
 print(resources.accounts)
 ```
 
-`resources.experiences` is a list of `Experience` objects, and `resources.accounts` is a list of `User` and `Group` objects. The example below will fetch resources, and send a messaging service message to every experience that was authorized.
+[`Resources.experiences`][rblxopencloud.Resources] is a list of [`rblxopencloud.Experience`][rblxopencloud.Experience] objects, and [`Resources.accounts`][rblxopencloud.Resources] is a list of [`rblxopencloud.User`][rblxopencloud.User] and [`rblxopencloud.Group`][rblxopencloud.Group] objects. The example below will fetch resources, and send a messaging service message to every experience that was authorized.
 ```py
 resources = access.fetch_resources()
 
@@ -117,20 +117,20 @@ for experience in resources.experiences:
 
 ### Refreshing Tokens
 
-Roblox Access Tokens only last for 15 minutes before expiring, once an Access Token has expired, it can no longer be used. However, instead of request authorization from the user every 15 minutes, you can refresh a token. When your app is first authorized, you must store the `access.refresh_token` somewhere where you'll be able to access it later. Anytime within 6 months of previously refreshing the token, you may use the `refresh_token` method to convert it to a new Access Token, like this example below:
+Roblox Access Tokens only last for 15 minutes before expiring, once an Access Token has expired, it can no longer be used. However, instead of request authorization from the user every 15 minutes, you can refresh a token. When your app is first authorized, you must store the [`AccessToken.refresh_token`][rblxopencloud.AccessToken] somewhere where you'll be able to access it later. Anytime within 6 months of previously refreshing the token, you may use [`OAuth2App.refresh_token`][rblxopencloud.OAuth2App.refresh_token] to convert it to a new Access Token, like this example below:
 
 ```py
 access = rblxapp.refresh_token("your stored refresh token")
 ```
 
-Note that the new access token will not have a `access.user`, and instead you'll have to use `access.fetch_userinfo()`. After refreshing the token, you will recieve a new refresh token which you must store in the old token's place. The old token is completly useless after it has been used.
+Note that the new access token will not have a [`AccessToken.user`][rblxopencloud.AccessToken], and instead you'll have to use [`AccessToken.fetch_userinfo()`][rblxopencloud.AccessToken.fetch_userinfo]. After refreshing the token, you will recieve a new refresh token which you must store in the old token's place. The old token is completly useless after it has been used.
 
 !!! warning
     After refreshing a token, the old token will no longer work. You must store the new token.
 
 ### Revoking Tokens
 
-When you're finished with an authorization and you won't need to use it anymore, you can revoke the token, and it's refresh token pair by using `access.revoke()`, here's an example:
+When you're finished with an authorization and you won't need to use it anymore, you can revoke the token, and it's refresh token pair by using [`AccessToken.revoke()`][rblxopencloud.AccessToken.revoke], here's an example:
 
 ```py
 access.revoke()
@@ -139,12 +139,12 @@ Users can still authorize your app again and you will be able to get a new acces
 
 ### Authorization with PKCE
 
-If your application can't keep a secret (the application's client secret can not be kept a secret from other parties), then you should use PKCE for extra security. Before you redirect a user, you should generate a code verifier using `generate_code_verifier`, and then pass it to the `generate_uri` in the `code_verifier` field, like this:
+If your application can't keep a secret (the application's client secret can not be kept a secret from other parties), then you should use PKCE for extra security. Before you redirect a user, you should generate a code verifier using [`OAuth2App.generate_code_verifier()`][rblxopencloud.OAuth2App.generate_code_verifier], and then pass it to the [`OAuth2App.generate_uri()`][rblxopencloud.OAuth2App.generate_uri] in the `code_verifier` field, like this:
 ```py
 code_verifier = rblxapp.generate_code_verifier()
 rblxapp.generate_uri(['openid', 'profile'], code_verifier=code_verifier)
 ```
-You will need to store the code verifier for when the user returns back from the authorization page. When the user does return from the authorization page, you must feed the code verifier with the code in the `exchange_code` function, like this:
+You will need to store the code verifier for when the user returns back from the authorization page. When the user does return from the authorization page, you must feed the code verifier with the code in [`OAuth2App.exchange_code()`][rblxopencloud.OAuth2App.exchange_code], like this:
 ```py
 access = rblxapp.exchange_code("examplecode", code_verifier=code_verifier)
 ```
--- rblxopencloud/user.py
+++ rblxopencloud/user.py
@@ -22,6 +22,64 @@ __all__ = (
 )
 
 class InventoryAssetType(Enum):
+    """
+    Enum denoting what type a [`rblxopencloud.InventoryAsset`][rblxopencloud.InventoryAsset] is.
+
+    Attributes:
+        Unknown (0): The asset type is unknown.
+        ClassicTShirt (1):
+        Audio (2):
+        Hat (3):
+        Model (4):
+        ClassicShirt (5):
+        ClassicPants (6):
+        Decal (7):
+        ClassicHead (8):
+        Face (9):
+        Gear (10):
+        Animation (11):
+        Torso (12):
+        RightArm (13):
+        LeftArm (14):
+        LeftLeg (15):
+        RightLeg (16):
+        Package (17):
+        Plugin (18):
+        MeshPart (19):
+        HairAccessory (20):
+        FaceAccessory (21):
+        NeckAccessory (22):
+        ShoulderAccessory (23):
+        FrontAccessory (24):
+        BackAccessory (25):
+        WaistAccessory (26):
+        ClimbAnimation (27):
+        DeathAnimation (28):
+        FallAnimation (29):
+        IdleAnimation (30):
+        JumpAnimation (31):
+        RunAnimation (32):
+        SwimAnimation (33):
+        WalkAnimation (34):
+        PoseAnimation (35):
+        EmoteAnimation (36):
+        Video (37):
+        TShirtAccessory (38):
+        ShirtAccessory (39):
+        PantsAccessory (40):
+        JacketAccessory (41):
+        SweaterAccessory (42):
+        ShortsAccessory (43):
+        LeftShoeAccessory (44):
+        RightShoeAccessory (45):
+        DressSkirtAccessory (46):
+        EyebrowAccessory (47):
+        EyelashAccessory (48):
+        MoodAnimation (49):
+        DynamicHead (50):
+        CreatedPlace (51):
+        PurchasedPlace (52):
+    """
     Unknown = 0
     ClassicTShirt = 1
     Audio = 2
@@ -77,6 +135,15 @@ class InventoryAssetType(Enum):
     PurchasedPlace = 52
 
 class InventoryItemState(Enum):
+    """
+    Enum representing wether the collectable item is in hold, or if the user can sell it.
+    
+    Attributes:
+        Unknown (1): The current state is unknown.
+        Available (2): The user could list the collectable for sale if they have premium.
+        Hold (3): The user recently purchased the collectable and can't be sold yet. 
+    """
+
     Unknown = 0
     Available = 1
     Hold = 2
@@ -144,6 +211,16 @@ state_type_strings = {
 }
 
 class InventoryItem():
+    """
+    Represents a base item in a user's inventory. This method is usually not returned, however it bases multiple other classes.
+
+    !!! warning
+        This class isn't designed to be created by users. It is returned by [`User.list_inventory()`][rblxopencloud.User.list_inventory].
+
+    Attributes:
+        id (int): The ID of the inventory item.
+    """
+
     def __init__(self, id) -> None:
         self.id: int = id
 
@@ -151,6 +228,22 @@ class InventoryItem():
         return f"rblxopencloud.InventoryItem(id={self.id}"
 
 class InventoryAsset(InventoryItem):
+    """
+    Represents a basic inventory item, such as avatar items, and development items.
+
+    !!! warning
+        This class isn't designed to be created by users. It is returned by [`User.list_inventory()`][rblxopencloud.User.list_inventory].
+
+    Attributes:
+        id (int): The ID of the inventory item.
+        type (InventoryAssetType): The asset's type.
+        instance_id (int): The unique ID of this asset's instance.
+        collectable_item_id (Optional[str]): A unique item UUID for collectables.
+        collectable_instance_id (Optional[str]): A unique instance UUID for collectables.
+        serial_number (Optional[int]): The serial number of the collectable.
+        collectable_state (Optional[InventoryItemState]): Wether the item is ready for sale or in hold.
+    """
+
     def __init__(self, data) -> None:
         super().__init__(data["assetId"])
         self.type: InventoryAssetType = InventoryAssetType(asset_type_strings.get(data["inventoryItemAssetType"], InventoryAssetType.Unknown))
@@ -166,6 +259,16 @@ class InventoryAsset(InventoryItem):
         return f"rblxopencloud.InventoryAsset(id={self.id}, type={self.type})"
 
 class InventoryBadge(InventoryItem):
+    """
+    Represents a badge in a user's inventory.
+
+    !!! warning
+        This class isn't designed to be created by users. It is returned by [`User.list_inventory()`][rblxopencloud.User.list_inventory].
+
+    Attributes:
+        id (int): The ID of the badge.
+    """
+
     def __init__(self, data) -> None:
         super().__init__(data["badgeId"])
     
@@ -173,6 +276,16 @@ class InventoryBadge(InventoryItem):
         return f"rblxopencloud.InventoryBadge(id={self.id})"
 
 class InventoryGamePass(InventoryItem):
+    """
+    Represents a game pass in a user's inventory.
+
+    !!! warning
+        This class isn't designed to be created by users. It is returned by [`User.list_inventory()`][rblxopencloud.User.list_inventory].
+
+    Attributes:
+        id (int): The ID of the game pass.
+    """
+
     def __init__(self, data) -> None:
         super().__init__(data["gamePassId"])
     
@@ -180,6 +293,16 @@ class InventoryGamePass(InventoryItem):
         return f"rblxopencloud.InventoryGamePass(id={self.id})"
 
 class InventoryPrivateServer(InventoryItem):
+    """
+    Represents a private server in a user's inventory.
+
+    !!! warning
+        This class isn't designed to be created by users. It is returned by [`User.list_inventory()`][rblxopencloud.User.list_inventory].
+
+    Attributes:
+        id (int): The ID of the private server item.
+    """
+    
     def __init__(self, data) -> None:
         super().__init__(data["privateServerId"])
     
@@ -188,11 +311,21 @@ class InventoryPrivateServer(InventoryItem):
 
 class User(Creator):
     """
-    Represents a user on Roblox. It is used to provide information about a user in OAuth2, and to upload assets to a user.
-    ### Paramaters
-    id: int - The user's ID.
-    api_key: str - Your API key created from [Creator Dashboard](https://create.roblox.com/credentials) with access to this user.
+    Represents a user on Roblox. It is used to provide information about a user in OAuth2, fetch information about a user, and access their resources.
+
+    Args:
+        id: The user's ID.
+        api_key: The API key created on the [Creator Dashboard](https://create.roblox.com/credentials) with access to the user.
+    
+    Attributes:
+        id (int): The user's ID. The `openid` scope is required for OAuth2 authorization.
+        username (Optional[str]): The user's username, only avalible from OAuth2 with the `profile` scope.
+        display_name (Optional[str]): The user's display name, only avalible from OAuth2 with the `profile` scope.
+        profile_uri (str): A URL to the user's profile on Roblox. The `openid` scope is required for OAuth2 authorization.
+        headshot_uri (Optional[str]): A URI to Roblox's CDN for the user's avatar headshot, only avalible from OAuth2 with the `profile` scope. Example value: `https://tr.rbxcdn.com/0f00ba3ab40808dbbbf3410a5a637d2e/150/150/AvatarHeadshot/Png`
+        created_at (Optional[datetime.datetime]): The timestamp the user created their account, only avalible from OAuth2 with the `profile` scope.
     """
+
     def __init__(self, id: int, api_key: str) -> None:
         self.username: Optional[str] = None
         self.id: int = id
@@ -206,6 +339,36 @@ class User(Creator):
         super().__init__(id, api_key, "User")
 
     def list_groups(self, limit: Optional[int]=None) -> Iterable["GroupMember"]:
+        """
+        Returns an Iterable of [`rblxopencloud.GroupMember`][rblxopencloud.GroupMember] for every group the user is a member of.
+
+        **The `openid` and `group:read` scopes are required for OAuth2 authorization.**
+
+        Example:
+            This will print every group the user is a member of, and their role ID in that group. 
+            ```py
+            for member in user.list_groups():
+                print(member.group, member.role_id)
+            ```
+            If you'd like the keys in a list, you can use the list method:
+            ```py
+            list(user.list_groups())
+            ```
+
+        Args:
+            limit: Will not return more groups than this number. Set to `None` for no limit.
+        
+        Returns:
+            An Iterable of group memberships.
+        
+        Raises:
+            InvalidKey: The API key isn't valid, doesn't have access to list data store keys, or is from an invalid IP address.
+            NotFound: The user does not exist.
+            RateLimited: You've exceeded the rate limits.
+            ServiceUnavailable: The Roblox servers ran into an error, or are unavailable right now.
+            rblx_opencloudException: Roblox returned an unexpected error.
+        """
+
         from .group import GroupMember
 
         filter = None
@@ -242,23 +405,42 @@ class User(Creator):
 
     def list_inventory(self, limit: Optional[int]=None, only_collectibles: Optional[bool]=False, assets: Optional[Union[list[InventoryAssetType], list[int], bool]]=None, badges: Optional[Union[list[int], bool]]=False, game_passes: Optional[Union[list[int], bool]]=False, private_servers: Optional[Union[list[int], bool]]=False) -> Iterable[Union[InventoryAsset, InventoryBadge, InventoryGamePass, InventoryPrivateServer]]:
         """
-        Interates `rblx-open-cloud.InventoryItem` for items in the user's inventory. If `only_collectibles`, `assets`, `badges`, `game_passes`, and `private_servers` are `False`, then all inventory items are returned.
-        
-        The example below would iterate through every item in the user's inventory.
-        
-        ```py
+        Returns an Iterable of [`rblxopencloud.InventoryItem`][rblxopencloud.InventoryItem] for every item in the user's inventory. If `only_collectibles`, `assets`, `badges`, `game_passes`, and `private_servers` are all `False`/`None`, then all inventory items are returned.
+
+        **The `openid` and `user.inventory-item:read` scopes are required for OAuth2 authorization.**
+
+        Example:
+            This will print every item in the user's inventory. 
+            ```py
             for item in user.list_inventory():
                 print(item)
-        ```
+            ```
+            If you'd like the items in a list, you can use the list method:
+            ```py
+            list(user.list_inventory())
+            ```
+
+        Args:
+            only_collectibles: Wether the only inventory assets iterated are collectibles (limited items). If `True` and `assets` is `None`, then `assets` will default to `True`.
+            assets: If this is `True`, then it will return all assets, if it is a list of IDs, it will only return assets with the provided IDs, and if it is a list of [`rblxopencloud.InventoryAssetType`][rblxopencloud.InventoryAssetType] then it will only return assets of these types.
+            badges: If this is `True`, then it will return all badges, but if it is a list of IDs, it will only return badges with the provided IDs.
+            game_passes: If this is `True`, then it will return all game passes, but if it is a list of IDs, it will only return game passes with the provided IDs.
+            private_servers: If this is `True`, then it will return all private servers, but if it is a list of IDs, it will only return private servers with the provided IDs.
+            limit: Will not return more groups than this number. Set to `None` for no limit.
         
-        The `user.inventory-item:read` scope is required if authorized via OAuth2`.
-        ### Parameters
-        limit: Optional[bool] - he maximum number of inventory items to iterate. This can be `None` to return all items.
-        only_collectibles: Optional[bool] - Wether the only inventory assets iterated are collectibles (limited items).
-        assets: Optional[Union[list[InventoryAssetType], list[int], bool]] - If this is `True`, then it will return all assets, if it is a list of IDs, it will only return assets with the provided IDs, and if it is a list of :class:`rblx-open-cloud.InventoryAssetType` then it will only return assets of these types.
-        badges: Optional[Union[list[int], bool]] - If this is `True`, then it will return all badges, but if it is a list of IDs, it will only return badges with the provided IDs.
-        game_passes: Optional[Union[list[int], bool]] - If this is `True`, then it will return all game passes, but if it is a list of IDs, it will only return game passes with the provided IDs.
-        private_servers: Optional[Union[list[int], bool]] - If this is `True`, then it will return all private servers, but if it is a list of IDs, it will only return private servers with the provided IDs.
+        Returns:
+            An Iterable of all the items in the user's inventory.
+        
+        Raises:
+            InvalidKey: The API key isn't valid, doesn't have access to list data store keys, or is from an invalid IP address.
+            PermissionDenied: The user's inventory is private, and you do not have authorization to access it.
+            NotFound: The user does not exist.
+            RateLimited: You've exceeded the rate limits.
+            ServiceUnavailable: The Roblox servers ran into an error, or are unavailable right now.
+            rblx_opencloudException: Roblox returned an unexpected error.
+
+        !!! note
+            If the user's inventory is private, then you need to gain consent from either OAuth2, or by owning the API key. Otherwise, this method will raise [`rblxopencloud.PermissionDenied`][rblxopencloud.PermissionDenied]!
         """
 
         filter_dict = {}
